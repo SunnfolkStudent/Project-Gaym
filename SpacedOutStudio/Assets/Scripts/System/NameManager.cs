6a1f4a968b2f4454aa8a3cbdc437ad4d0d201bd9
Tmmrvg <Oetom01@outlook.com>
2022-04-26 14:35:32 +0200

    Keyboard WIP

--- SpacedOutStudio/Assets/Scripts/System/NameManager.cs
+++ SpacedOutStudio/Assets/Scripts/System/NameManager.cs
@@ -1,18 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public new string name;
+    
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        throw new NotImplementedException();
     }
+    
+    
 }
