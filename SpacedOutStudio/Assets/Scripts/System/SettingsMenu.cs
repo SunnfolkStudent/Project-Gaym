ffb9254f8f35d6a357f9bda83ef2eebab531324e
Resonid <erlend.kaarigstad@lyse.net>
2022-05-08 14:20:06 +0200

    stuffs

--- SpacedOutStudio/Assets/Scripts/System/SettingsMenu.cs
+++ SpacedOutStudio/Assets/Scripts/System/SettingsMenu.cs
@@ -10,19 +10,23 @@ namespace System
         public Slider masterSlider;
         public Slider musicSlider;
         public Slider sfxSlider;
+        public bool wait;
 
         private void Start()
         {
+            wait = true;
             mixer.GetFloat("masterVol", out var masterVol);
             masterSlider.value = masterVol;
             mixer.GetFloat("musicVol", out var musicVol);
             musicSlider.value = musicVol;
             mixer.GetFloat("sfxVol", out var sfxVol);
             sfxSlider.value = sfxVol;
+            wait = false;
         }
 
         public void UpdateVolume()
         {
+            if (wait) return;
             mixer.SetFloat("masterVol", masterSlider.value);
             mixer.SetFloat("musicVol", musicSlider.value);
             mixer.SetFloat("sfxVol", sfxSlider.value);
